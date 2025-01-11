be9aecfdabfda1f0cb59fefbe08d67b081775b5b
Ali Abbas <155300259+Ali-Abbas-CSE@users.noreply.github.com>
2025-01-11 18:08:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+var mobNav =document.querySelector(".mob-nav");
+var ham = document.querySelector("#ham");
+var count =0
+ham.addEventListener("click", function(){
+    if (count ===0) {
+        mobNav.style.display = "flex";
+        count =1;
+    }else{
+        mobNav.style.display = "none";
+        count =0;
+    }
+})
 
+
+
+// Typing effect code
+
+
+
+
+      var typed = new Typed("#element", {
+        strings: ["<i>a passionated Web developer.</i>", " a student."],
+        // typeSpeed: 50,
+        // loop: !0,
+        typeSpeed: 80,
+        backSpeed: 75,
+        startDelay: 1000,
+        backDelay: 2000,
+        loop: !0,
+        loopcount: false,
+        showCursor: false,
+      });
+    
