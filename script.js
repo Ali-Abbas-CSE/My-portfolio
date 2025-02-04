7b58a5e2b124939500a1857d8a4a8df0d262cc48
Ali Abbas <155300259+Ali-Abbas-CSE@users.noreply.github.com>
2025-02-04 23:21:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-var mobNav =document.querySelector(".mob-nav");
+var mobNav = document.querySelector(".mob-nav");
 var ham = document.querySelector("#ham");
-var count =0
-ham.addEventListener("click", function(){
-    if (count ===0) {
+var count = 0
+ham.addEventListener("click", function () {
+    if (count === 0) {
         mobNav.style.display = "flex";
-        count =1;
-    }else{
+        count = 1;
+    } else {
         mobNav.style.display = "none";
-        count =0;
+        count = 0;
     }
 })
 
 
 
 // Typing effect code
+var typed = new Typed("#element", {
+    strings: ["<i>a passionated Web developer.</i>", " a student."],
+    // typeSpeed: 50,
+    // loop: !0,
+    typeSpeed: 80,
+    backSpeed: 75,
+    startDelay: 1000,
+    backDelay: 2000,
+    loop: !0,
+    loopcount: false,
+    showCursor: false,
+});
 
 
 
 
-      var typed = new Typed("#element", {
-        strings: ["<i>a passionated Web developer.</i>", " a student."],
-        // typeSpeed: 50,
-        // loop: !0,
-        typeSpeed: 80,
-        backSpeed: 75,
-        startDelay: 1000,
-        backDelay: 2000,
-        loop: !0,
-        loopcount: false,
-        showCursor: false,
-      });
-    
